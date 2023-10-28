5f9c587f9ec237750c42ddcd4c37709fc23f220d
jrheard <jrheard@cs.stanford.edu>
2023-10-28 15:27:06 -0700

    all done with day one, happy w the result

--- day_1.rb
+++ day_1.rb
@@ -1,45 +1,38 @@
 # typed: true
 
-# TODO: is this main class necessary???
-class Main
-  extend T::Sig
+require 'sorbet-runtime'
 
-  sig { returns(T::Array[Integer]) }
-  def load_input
-    File.readlines('inputs/day_1.txt', chomp: true).map do |line|
-      Integer(line)
-    end
-  end
+extend T::Sig
 
-  # "Count the number of times a depth measurement increases from the previous
-  # measurement. (There is no measurement before the first measurement.) "
-  sig { returns(Integer) }
-  def part_one
-    depths = load_input
-
-    depths.zip(T.must(depths[1..])).select do |depth_one, depth_two|
-      !depth_two.nil? && depth_two > depth_one
-    end.count
-  end
-
-  sig { returns(Integer) }
-  def part_two
-    depths = load_input
-
-    last_window_sum = T.must(depths[0..2]).sum
-    result = 0
-    depths.each_cons(3).each do |window|
-      result += 1 if window.sum > last_window_sum
-
-      last_window_sum = window.sum
-    end
-
-    result
-
-    # TODO: can i do this with each_cons?
+sig { returns(T::Array[Integer]) }
+def self.load_input
+  File.readlines('inputs/day_1.txt', chomp: true).map do |line|
+    Integer(line)
   end
 end
 
-main = Main.new
-puts main.part_one
-puts main.part_two
+# "Count the number of times a depth measurement increases from the previous
+# measurement. (There is no measurement before the first measurement.) "
+sig { returns(Integer) }
+def self.part_one
+  depths = load_input
+
+  depths.zip(T.must(depths[1..])).select do |depth_one, depth_two|
+    !depth_two.nil? && depth_two > depth_one
+  end.count
+end
+
+# "Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?"
+sig { returns(Integer) }
+def self.part_two
+  depths = load_input
+
+  window_sums = depths.each_cons(3).map(&:sum)
+
+  window_sums.zip(T.must(window_sums[1..])).select do |window_one, window_two|
+    !window_two.nil? && window_two > window_one
+  end.count
+end
+
+puts part_one
+puts part_two
