126ee43905ed0d6649508bcfb24cc3607db0036d
jrheard <jrheard@cs.stanford.edu>
2023-10-28 16:00:16 -0700

    WIP

--- day_2.rb
+++ day_2.rb
@@ -5,36 +5,9 @@ require 'sorbet-runtime'
 module DayTwo
   extend T::Sig
 
-  # A representation of an input line like `forward 5`.
-  class Instruction
-    extend T::Sig
-
-    sig { returns(Symbol) }
-    attr_reader :direction
-
-    sig { returns(Integer) }
-    attr_reader :magnitude
-
-    sig { params(direction: Symbol, magnitude: Integer).void }
-    def initialize(direction, magnitude)
-      @direction = direction
-      @magnitude = magnitude
-    end
-
-    sig { returns(String) }
-    def to_s
-      "#{direction} #{magnitude}"
-    end
-  end
-
-  sig { returns(T::Array[Instruction]) }
+  sig { returns(T::Array[String]) }
   def self.load_input
-    File.readlines('inputs/day_2.txt', chomp: true).map do |line|
-      Instruction.new(
-        T.must(line.split[0]).to_sym,
-        Integer(T.must(line.split[1]))
-      )
-    end
+    File.readlines('inputs/day_2.txt', chomp: true)
   end
 
   # "Calculate the horizontal position and depth you would have after following
@@ -42,17 +15,19 @@ module DayTwo
   # position by your final depth?"
   sig { returns(Integer) }
   def self.part_one
-    # TODO: rewrite without Instruction, just pattern matching on string.split
     x = 0
     y = 0
-    load_input.each do |instruction|
-      case instruction.direction
-      when :forward
-        x += instruction.magnitude
-      when :down
-        y += instruction.magnitude
-      when :up
-        y -= instruction.magnitude
+    load_input.each do |line|
+      # Lines look like "forward 5".
+      magnitude = Integer(T.must(line.split[1]))
+
+      case T.must(line.split[0])
+      when 'forward'
+        x += magnitude
+      when 'down'
+        y += magnitude
+      when 'up'
+        y -= magnitude
       end
     end
 
