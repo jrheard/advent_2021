894079895d9ad7bc7ee825573122213727df09d7
jrheard <jrheard@cs.stanford.edu>
2023-10-29 13:41:16 -0700

    WIP

--- day_4.rb
+++ day_4.rb
@@ -5,6 +5,7 @@ require 'sorbet-runtime'
 module DayFour
   extend T::Sig
 
+  # "Bingo is played on a set of boards each consisting of a 5x5 grid of numbers."
   class Board
     extend T::Sig
 
@@ -23,6 +24,54 @@ module DayFour
       end, T::Array[Integer])
 
       @marked_positions = T.let([], T::Array[[Integer, Integer]])
+    end
+
+    # "Numbers are chosen at random, and the chosen number is marked on all
+    # boards on which it appears. (Numbers may not appear on all boards.)"
+    sig { params(num: Integer).void }
+    def mark_number(num)
+      index = @contents.index(num)
+      return unless index
+
+      @marked_positions << [
+        index % 5,
+        index / 5
+      ]
+    end
+
+    # "If all numbers in any row or any column of a board are marked, that board wins. (Diagonals don't count.)"
+    sig { returns(T::Boolean) }
+    def won?
+      5.times do |i|
+        return true if @marked_positions.select { |x, _y| x == i }.count == 5
+        return true if @marked_positions.select { |_x, y| y == i }.count == 5
+      end
+
+      false
+    end
+
+    # "The score of the winning board can now be calculated. Start by finding
+    # the sum of all unmarked numbers on that board. Then, multiply that sum by
+    # the number that was just called when the board won to get the final score."
+    sig { returns(T.nilable(Integer)) }
+    def score
+      return nil unless won?
+
+      positions = (0..24).map do |i|
+        [i % 5, i / 5]
+      end
+      unmarked_positions_and_indexes = positions.each_with_index.reject do |position, _i|
+        @marked_positions.include?(position)
+      end
+      marked_positions_and_indexes = positions.each_with_index.select do |position, _i|
+        @marked_positions.include?(position)
+      end
+
+      T.must(marked_positions_and_indexes.map do |_position, i|
+        @contents[i]
+      end.sum) * T.must(unmarked_positions_and_indexes.map do |_position, i|
+                          @contents[i]
+                        end.sum)
     end
   end
 
