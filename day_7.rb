a25e96468b5a01beb3c4f64c6381b9c229db12c2
jrheard <jrheard@cs.stanford.edu>
2023-11-02 12:50:26 -0700

    7 part 1

---
+++ day_7.rb
@@ -0,0 +1,42 @@
+# typed: strict
+
+require 'sorbet-runtime'
+
+module DaySeven
+  extend T::Sig
+
+  # thx stackoverflow
+  sig { params(array: T::Array[Integer]).returns(Integer) }
+  def self.median(array)
+    raise if array.empty?
+
+    sorted = array.sort
+    len = sorted.length
+    (sorted.fetch((len - 1) / 2) + sorted.fetch(len / 2)) / 2
+  end
+
+  sig { returns(T::Array[Integer]) }
+  def self.load_input
+    line = File.readlines('inputs/day_7.txt', chomp: true).fetch(0)
+    line.split(',').map do |num|
+      Integer(num)
+    end
+  end
+
+  sig { returns(Integer) }
+  def self.part_one
+    crab_positions = load_input
+    best_position = median(crab_positions)
+    crab_positions.map do |position|
+      (best_position - position).abs
+    end.sum
+  end
+
+  sig { returns(Integer) }
+  def self.part_two
+    -1
+  end
+end
+
+puts DaySeven.part_one
+puts DaySeven.part_two
